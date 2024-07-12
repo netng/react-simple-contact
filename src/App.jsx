d70eafa1d81a3706b28454df64963262871bd0de
nandang <net.nandang@gmail.com>
2024-07-12 14:51:52 +0700

    Update App.jsx with companion component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
-function App() {
+import { useState } from "react"
+import { initialContacts } from "./contacts.js"
+import EditContact from "./EditContact.jsx";
 
+function App() {
+  const [contacts, setContacts] = useState(initialContacts);
+  const [selectedContactId, setSelectedContactId] = useState(1);
+
+  const selectedContact = contacts.find((contact) => contact.id === selectedContactId); 
+
+  function handleUpdateContact(updatedContact) {
+    setContacts(
+      contacts.map((contact) => {
+        if (contact.id === updatedContact.id) {
+          return {
+            ...contact,
+            name: updatedContact.name,
+            email: updatedContact.email
+          };
+        } else {
+          return contact;
+        }
+      })
+    )
+  }
+  
   return (
     <>
+      { contacts.map((contact) => (
+        <button 
+          key={contact.id}
+          onClick={() => setSelectedContactId(contact.id)}>
+          { contact.id === selectedContactId ? 
+            <b>{contact.name}</b> : 
+            contact.name
+          }
+        </button>
+      ))}
+      <hr />
 
+      <EditContact 
+        selectedContact={selectedContact}
+        onSave={(updatedContact) => handleUpdateContact(updatedContact)} 
+      />
     </>
   )
 }
