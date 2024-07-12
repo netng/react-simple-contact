fa2751e12acbca394b5f96622c51d7a7b1e08ea0
nandang <net.nandang@gmail.com>
2024-07-12 14:49:52 +0700

    add initial contacts data

---
+++ src/contacts.js
@@ -0,0 +1,5 @@
+export const initialContacts = [
+    { id: 1, name: 'Taylor', email: 'taylor@mail.com' },
+    { id: 2, name: 'Alice', email: 'alice@mail.com' },
+    { id: 3, name: 'Bob', email: 'bob@mail.com' },
+]
